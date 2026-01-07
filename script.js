cb1107234be37a9bf951541268e0625d3fb2d259
Ondřej Ilek <7ondik2007@gmail.com>
2026-01-07 23:54:44 +0100

    moc pekny to je

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const starttlacitko = document.getElementById("starttlacitko");
 const hero = document.querySelector(".hero");
 const poll = document.querySelector(".poll");
-
+const bg = document.querySelector(".bg");
 const welcome = document.querySelector(".welcome");
 
 starttlacitko.addEventListener("click", () => {
   welcome.classList.add("hide");
+  starttlacitko.classList.add("hidden-tlacitko")
   poll.classList.add("show");
+  bg.classList.add("blur");
 });
 
