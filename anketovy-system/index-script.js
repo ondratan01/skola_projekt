84e5822997674a5f2ca0b845997f160ea60d1885
Ondřej Ilek <7ondik2007@gmail.com>
2026-01-16 01:38:42 +0100

    moc pekne to je ted

---
+++ anketovy-system/index-script.js
@@ -0,0 +1,11 @@
+
+
+
+
+/* 
+<select class="question-type">
+  <option value="single">Jedna odpověď</option>
+  <option value="multiple">Více odpovědí</option>
+  <option value="text">Textová odpověď</option>
+</select>
+*/
