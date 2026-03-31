8303d15fce57899e4446729650945cf0158a62fd
Ondřej Ilek <7ondik2007@gmail.com>
2026-04-01 00:17:00 +0200

    proc to sakra delam tak dlouho

--- anketovy-system/vytvorit-script.js
+++ anketovy-system/vytvorit-script.js
@@ -1,28 +1,79 @@
-export const questions = [];
-
+ const questions = [];
+ let moznosti;
+ let check;
 const nahledContent = document.getElementById("nahledContent");
 
 document.getElementById("addQuestion").addEventListener("click", () => {
   const text = document.getElementById("questionText").value;
   const type = document.getElementById("questionType").value;
-  const moznosti = document.getElementById("moznosti").value.split(",");
 
-  questions.push({text, type, moznosti});
+  if(type == "text")
+  {
+    moznosti = [];
+    check = 0
+  }
+  if(type == "checkbox")
+  {
+    moznosti = document.getElementById("moznosti").value.split(",");
+    check=1
+  }
+  if(type == "radio")
+  {
+    moznosti = document.getElementById("moznosti").value.split(",");
+    check=2
+  }
+
+
+  
+  
+
+  questions.push({text, type, moznosti, check});
 });
 
 document.getElementById("updatePreview").addEventListener("click", () => {
   nahledContent.innerHTML = "";
+
+
+  
 
   questions.forEach(x => {
     const div = document.createElement("div");
     div.innerHTML = `<p><strong>${x.text}</strong></p>`;
     nahledContent.appendChild(div);
 
-    x.moznosti.forEach(option => {
+
+    if(x.check == 1)
+    {
+      const info = document.createElement("div");
+      info.innerHTML = "(více odpovědí) <br> <br>";
+      div.appendChild(info);
+    }
+
+    if(x.check == 2)
+    {
+      const info = document.createElement("div");
+      info.innerHTML = "(jedna odpověď) <br> <br>";
+      div.appendChild(info);
+    }
+
+    if(x.check == 0)
+    {
+      const info = document.createElement("div");
+      info.innerHTML = "(text odpověď) <br> <br>";
+      div.appendChild(info);
+    }
+
+
+    if(x.check != 0)
+    {
+      x.moznosti.forEach(option => {
     const opt = document.createElement("div");
      opt.textContent = "- " + option;
      div.appendChild(opt);
                                 });
+    }
+    
+
                          });
                                                                          });
 
@@ -30,4 +81,25 @@ document.getElementById("removeLast").addEventListener("click", () => {
   questions.pop();
 });
 
+
+document.getElementById("savePoll").addEventListener("click", () => {
+  const data = JSON.stringify(questions,null,2);
+
+  const blob = new Blob([data], {type: "application/json"});
+
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+
+  a.download = "anketa.json";
+
+  a.click();
+
+
+
+
+
+
+
+
+})
 
